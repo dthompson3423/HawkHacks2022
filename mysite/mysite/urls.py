cae1e9bfccba9be3e489ad0bd4d108e1424822d7
jmedeiros11 <jmedeiros1011@gmail.com>
2022-05-13 21:00:32 -0400

    updating urls

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,5 +17,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('admin/', admin.site.urls)
+    
 ]
